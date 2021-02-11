1ce9e6e5dcc0dd1b9ba915dac10c6cbf0f604cbb
poornima k <41606774+poornimakumar@users.noreply.github.com>
2021-02-11 18:59:44 +0530

    Update BaseTest.java

--- Poornima_Assessment/src/test/java/com/qa/assessment/TestClass/BaseTest.java
+++ Poornima_Assessment/src/test/java/com/qa/assessment/TestClass/BaseTest.java
@@ -1,31 +1,31 @@
-package com.qa.assessment.TestClass;
+// package com.qa.assessment.TestClass;
 
-import java.io.IOException;
+// import java.io.IOException;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.chrome.ChromeDriver;
+// import org.testng.annotations.AfterClass;
+// import org.testng.annotations.BeforeClass;
 
-import com.qa.assessment.FileHandling.PropertyManager;
+// import com.qa.assessment.FileHandling.PropertyManager;
 
 
 
-public class BaseTest {
-	protected	WebDriver driver;
-	@BeforeClass
-	public void setUp() throws IOException
-	{
-		System.setProperty("webdriver.chrome.driver", PropertyManager.getProperty("path.driver.chrome"));//accessing the web driver from property manager
-		driver = new ChromeDriver();
-		driver.get(PropertyManager.getProperty("url.app"));// accessing the url for testing
-	}
+// public class BaseTest {
+// 	protected	WebDriver driver;
+// 	@BeforeClass
+// 	public void setUp() throws IOException
+// 	{
+// 		System.setProperty("webdriver.chrome.driver", PropertyManager.getProperty("path.driver.chrome"));//accessing the web driver from property manager
+// 		driver = new ChromeDriver();
+// 		driver.get(PropertyManager.getProperty("url.app"));// accessing the url for testing
+// 	}
 	
 	
 	
-	@AfterClass
-	public void tearDown()
-	{
-		driver.close();
-}
-}
+// 	@AfterClass
+// 	public void tearDown()
+// 	{
+// 		driver.close();
+// }
+// }
